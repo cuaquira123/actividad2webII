b051cfc8ef42174bea8dd2d304f4075bd10f2a75
cuaquira123 <rudycuaqyura@gmail.com>
2024-12-07 22:16:56 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { CapitalizadoPipe } from './pipe/capitalizado.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    InicioComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule,
